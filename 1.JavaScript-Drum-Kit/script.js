b95c7cf636d5d5ca1a362cdd0f7491e09d6cd0af
anhle <anhle12892@gmail.com>
2017-02-08 00:39:56 +0700

    done day 1

---
+++ 1.JavaScript-Drum-Kit/script.js
@@ -0,0 +1,24 @@
+function playSount(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+function removeTransition(event) {
+    if (event.propertyName != 'transform') return;
+
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(function (key) {
+    key.addEventListener('transitionend', removeTransition);
+}, this);
+
+window.addEventListener('keydown', playSount);
